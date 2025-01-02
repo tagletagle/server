fca7c50eefe61a84cc83ff5ea8f00a58d545954d
eclipse021 <yahang2008@naver.com>
2025-01-02 20:27:16 +0900

    feat: spring boot 생성 및 gradle 의존성 설정

---
+++ src/main/java/com/example/tagletagle/TagletagleApplication.java
@@ -0,0 +1,13 @@
+package com.example.tagletagle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TagletagleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TagletagleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tagletagle/TagletagleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tagletagle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TagletagleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
