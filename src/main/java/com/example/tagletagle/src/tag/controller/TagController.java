771fa8331872f2d18af223e462f8b427a0d42d5d
koisu24 <nissy930@naver.com>
2025-01-21 13:20:48 +0900

    feat. 전체 태그 목록 조회

--- src/main/java/com/example/tagletagle/src/tag/controller/TagController.java
+++ src/main/java/com/example/tagletagle/src/tag/controller/TagController.java
@@ -1,10 +1,23 @@
 package com.example.tagletagle.src.tag.controller;
 
+import com.example.tagletagle.src.tag.dto.TagDTO;
+import com.example.tagletagle.src.tag.service.TagService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 public class TagController {
+
+    private final TagService tagService;
+
+    @GetMapping("/api/user/tag/fullList")
+    public List<TagDTO> getFullTagList() {
+        return tagService.findAllTag();
+
+    }
 }
---
+++ src/main/java/com/example/tagletagle/src/tag/dto/TagDTO.java
@@ -0,0 +1,14 @@
+package com.example.tagletagle.src.tag.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+
+public class TagDTO {
+    private Long id;
+    private String name;
+}
